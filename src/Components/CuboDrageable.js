67ae419bf65395df4011206c31300f9765c293fb
Francisco <Fecsoul@gmail.com>
2023-10-22 02:17:46 -0300

    quinto commit

--- src/Components/CuboDrageable.js
+++ src/Components/CuboDrageable.js
@@ -2,17 +2,22 @@ import React, { useRef, useState } from 'react';
 
 function CuboDrageable() {
   const [isDragging, setIsDragging] = useState(false);
+  const [isSelected, setisSelected] = useState('blue');
+
   const cubeRef = useRef();
   const previousPosition = useRef([0, 0]);
   const sensitivity = 0.05; // Ajusta la sensibilidad según tus preferencias
 
   const handlePointerDown = (e) => {
     setIsDragging(true);
+    setisSelected('green')
     previousPosition.current = [e.clientX, e.clientY];
   };
 
   const handlePointerUp = () => {
     setIsDragging(false);
+    setisSelected('blue')
+
   };
 
   const handlePointerMove = (e) => {
@@ -23,7 +28,9 @@ function CuboDrageable() {
 
       const newPosition = [
         cubeRef.current.position.x + deltaX,
-        cubeRef.current.position.y - deltaY, // Invertir la dirección del eje Y si es necesario
+        cubeRef.current.position.y - deltaY, 
+        cubeRef.current.position.z + deltaX
+        // Invertir la dirección del eje Y si es necesario
       ];
 
       cubeRef.current.position.x = newPosition[0];
@@ -47,7 +54,7 @@ function CuboDrageable() {
       onPointerMove={handlePointerMove}
     >
       <boxGeometry args={[4, 4, 4]} />
-      <meshBasicMaterial color="blue" />
+      <meshBasicMaterial color={isSelected} />
     </mesh>
   );
 }
